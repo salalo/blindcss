362ab24a4b51792b22a6c4918d9a34af3d40dc9a
Wojciech Sala <wojteksala190@gmail.com>
2019-04-23 21:34:40 +0200

    connecting to db

--- server/src/app.js
+++ server/src/app.js
@@ -1,21 +1,36 @@
-import express from 'express'
-import morgan from 'morgan'
-import bodyParser from 'body-parser'
-import { join } from 'path'
-import cors from 'cors'
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import { join } from 'path';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const app = express()
+import auth from './routes/auth.js';
+import keys from './config/keys.js';
 
-app.use(cors({
-  credentials: true,
-  origin: 'http://localhost:8080'
-}))
+const app = express();
 
-app.set('views', join(__dirname, 'views'))
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(morgan('dev'))
-app.set('trust proxy', 1) // trust first proxy
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:8080'
+  })
+);
 
-app.listen(process.env.PORT || 8081)
+// connect to database
+mongoose.connect(keys.mongodb.DB, { useNewUrlParser: true }).then(
+  () => {
+    console.log('\nConnected successfully!');
+  },
+  err => console.log('\nCan not connect to the database\n\n' + err)
+);
+
+app.set('views', join(__dirname, 'views'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.set('trust proxy', 1); // trust first proxy
+// app.use('/auth', auth());
+
+app.listen(process.env.PORT || 8081);
---
+++ server/src/config/keys.js
@@ -0,0 +1,5 @@
+export default {
+  mongodb: {
+    DB: 'mongodb://localhost:27017/users'
+  }
+};
